ddfbb02c25a14e47fc38f467cf8d3b87187c746e
mr-dj06 <mr.learner06@gmail.com>
2025-06-18 13:16:53 +0530

    Fix path errors for case-sensitive environments

--- webiso-frontend/src/pages/About/Aboutus.jsx
+++ webiso-frontend/src/pages/About/Aboutus.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Title from '../../components/Title.jsx'
 import About from '../../components/About.jsx'
 import Slider from '../../components/Slider.jsx'
-import Start from '../../components/start.jsx'
+import Start from '../../components/Start.jsx'
 import Team from '../../components/Team.jsx'
 
 function Aboutus() {
