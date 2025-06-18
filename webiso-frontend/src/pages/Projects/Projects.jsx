3eef78dcb39616c10e394c6644cd68bbaa1cf44b
mr-dj06 <mr.learner06@gmail.com>
2025-06-18 13:13:48 +0530

    Fix path for case-sensitive environments

--- webiso-frontend/src/pages/Projects/Projects.jsx
+++ webiso-frontend/src/pages/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import Title from '../../components/Title.jsx'
 import Products from '../../components/Products.jsx'
 import Slider from '../../components/Slider.jsx'
-import Start from '../../components/start.jsx'
+import Start from '../../components/Start.jsx'
 import Review from '../../components/Review.jsx'
 
 function Projects() {
